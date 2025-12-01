0d9cc5e9bc68e07b84827b61fd6cf72abaceebd8
janmatthewjavier <janmatthewjavier08@gmail.com>
2025-12-01 15:29:56 +0800

    feat/insert-10-rows-doctors

--- veterinary.sql
+++ veterinary.sql
@@ -98,5 +98,13 @@ VALUES
     (9,  5,  '2025-11-06', 'Post-surgery evaluation'),
     (10, 6,  '2025-11-07', 'General health check-up');
 
+INSERT INTO doctors(doctorid, dfirstname, dlastname, speciality, phone, email)
+VALUES
+(1, 'Dr.Maria', 'Santos', 'General Veterinarian', '987-654-3210', 'maria@example.com'),
+(2, 'Dr.Antonio', 'Gonzales', 'Feline Specialist', '555-123-4567', 'antonio@example.com'),
+(3, 'Dr.Felipe', 'Luna', 'Orthopedic Specialist', '111-222-3333', 'felipe@example.com'),
+(4, 'Dr.Sofia', 'Reyes', 'Dermatology Specialist', '999-888-7777', 'sofia@xample.com'),
+(5, 'Dr.Luis', 'Torres', 'Surgery Specialist', '123-555-7777', 'luis@example.com'),
+(6, 'Dr.Carmen', 'Fernandez', 'Opthalmology Specialist', '333-222-1111', 'carmen@example.com');
 
 
